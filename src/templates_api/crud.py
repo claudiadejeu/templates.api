aa16348aca4b1f332c81f6df9ece72eb38614a63
claudejeuboronat <116017198+claudejeuboronat@users.noreply.github.com>
2024-08-31 13:57:01 +0100

    Add PUT, PATCH, DELETE, and Search endpoints (#3)
    - Add update item, delete item, partial update item, search items by query
    - Add ItemUpdate schema
    - Add config with baseurl settings
    - Modify linting tools to use Ruff instead of isort, black and flake8
    - Tests to be added on next PR

--- src/templates_api/crud.py
+++ src/templates_api/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -12,8 +13,44 @@ def get_items(db: Session, skip: int = 0, limit: int = 10):
 
 
 def create_item(db: Session, item: schemas.ItemCreate):
-    db_item = models.Item(name=item.name, description=item.description)
+    db_item = models.Item(
+        name=item.name,
+        description=item.description,
+        price=item.price,
+        quantity=item.quantity,
+    )
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def delete_item(db: Session, item_id: int):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+    return db_item
+
+
+def update_item(db: Session, item_id: int, item: schemas.ItemUpdate):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if db_item:
+        for key, value in item.model_dump(exclude_unset=True).items():
+            setattr(db_item, key, value)
+        db.commit()
+        db.refresh(db_item)
+    return db_item
+
+
+def search_items(db: Session, query: str):
+    return (
+        db.query(models.Item)
+        .filter(
+            or_(
+                models.Item.name.ilike(f"%{query}%"),
+                models.Item.description.ilike(f"%{query}%"),
+            )
+        )
+        .all()
+    )
